3e2b3636ba5c3ef825721f204f42e5901e278fcc
Rosemoe <2073412493@qq.com>
2025-03-23 01:58:29 +0800

    feat(deps): update dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.serialization") version "1.9.23"
+    kotlin("jvm") version "2.1.20"
+    kotlin("plugin.serialization") version "2.1.20"
 }
 
 group = "com.itoken.team.wutspider"
@@ -11,9 +11,9 @@ repositories {
 }
 
 dependencies {
-    implementation("com.squareup.okhttp3:okhttp:4.9.0") // Currently used by Android client
+    implementation("com.squareup.okhttp3:okhttp:4.12.0") // Currently used by Android client
     implementation("org.json:json:20240303") //FIXME remove this dependency on Android
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
 
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
