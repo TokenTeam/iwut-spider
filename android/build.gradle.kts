52d45743c0a5176f7226903080c5126bf0ca59e8
Rosemoe <2073412493@qq.com>
2025-03-25 16:03:14 +0800

    feat: replace `org.json` library with gson

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0") // Currently used by Android client
-    implementation("org.json:json:20240303") //FIXME remove this dependency on Android
+    implementation("com.google.code.gson:gson:2.12.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
 
     testImplementation(kotlin("test"))
--- android/src/main/kotlin/com/itoken/team/wutspider/impl/parser/payload/JsonPayloadParser.kt
+++ android/src/main/kotlin/com/itoken/team/wutspider/impl/parser/payload/JsonPayloadParser.kt
@@ -1,29 +1,47 @@
 package com.itoken.team.wutspider.impl.parser.payload
 
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import com.itoken.team.wutspider.SpiderException
 import com.itoken.team.wutspider.interfaces.IPayloadParser
 import com.itoken.team.wutspider.model.SpiderKeyValuePair
 import com.itoken.team.wutspider.model.SpiderValueType
 import com.itoken.team.wutspider.util.fillVariables
-import org.json.JSONObject
 
 class JsonPayloadParser : IPayloadParser {
 
     override var context: MutableMap<String, String>? = null
 
-    override fun parse(pattern: String?, value: List<SpiderKeyValuePair>): String {
-        return JSONObject().also { json ->
+    override fun parse(
+        pattern: String?,
+        value: List<SpiderKeyValuePair>
+    ): String {
+        return JsonObject().also { json ->
             value.forEach { pair ->
-                val actualValue = pair.value.fillVariables(context ?: throw SpiderException("Context is null"))
+                val actualValue = pair.value.fillVariables(
+                    context ?: throw SpiderException("Context is null")
+                )
                 when (pair.type) {
-                    SpiderValueType.STRING -> json.put(pair.key, actualValue)
-                    SpiderValueType.NUMBER -> json.put(pair.key,
-                        actualValue.toDoubleOrNull()
+                    SpiderValueType.STRING -> json.addProperty(
+                        pair.key,
+                        actualValue
+                    )
+
+                    SpiderValueType.NUMBER -> json.addProperty(
+                        pair.key,
+                        actualValue.toLongOrNull()
+                            ?: actualValue.toDoubleOrNull()
                             ?: throw SpiderException("can not interpret '${pair.value}' as a number")
                     )
 
-                    SpiderValueType.BOOLEAN -> json.put(pair.key, actualValue.toBoolean())
-                    SpiderValueType.OBJECT -> json.put(pair.key, JSONObject(actualValue))
+                    SpiderValueType.BOOLEAN -> json.addProperty(
+                        pair.key,
+                        actualValue.toBoolean()
+                    )
+
+                    SpiderValueType.OBJECT -> json.add(
+                        pair.key, JsonParser.parseString(actualValue)
+                    )
                 }
             }
         }.toString()
